21ebf8ee757210dbacbf668b628e8085c3052ca2
makoto.mase <makoto.mase@gmail.com>
2024-11-26 15:00:44 +0900

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.12.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "ca-masem-test"
+  region  = "asia-northeast1"
+}
+
+data "google_project" "main" {
+}
+
+resource "google_storage_bucket" "static-site" {
+  name                        = "${data.google_project.main.project_id}-mediamtx"
+  location                    = "asia-northeast1"
+  uniform_bucket_level_access = true
+}
+
+resource "google_artifact_registry_repository" "main" {
+  location      = "asia-northeast1"
+  repository_id = "jaguer-winter"
+  format        = "DOCKER"
+}
+
+resource "google_compute_network" "main" {
+  name                    = "jaguer-winter"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "main" {
+  name          = "jaguer-winter-tokyo"
+  ip_cidr_range = "192.168.0.0/24"
+  network       = google_compute_network.main.id
+
+  secondary_ip_range {
+    range_name    = "pod"
+    ip_cidr_range = "10.0.0.0/16"
+  }
+  secondary_ip_range {
+    range_name    = "svc"
+    ip_cidr_range = "10.1.0.0/16"
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "allow-ingress-mediamtx"
+  network = google_compute_network.main.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1935", "8554"]
+  }
+
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+}
+
+resource "google_container_cluster" "main" {
+  name = "jaguer-winter"
+
+  enable_autopilot = true
+  network          = google_compute_network.main.id
+  subnetwork       = google_compute_subnetwork.main.id
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "pod"
+    services_secondary_range_name = "svc"
+  }
+}
+
+resource "google_project_iam_binding" "main" {
+  project = data.google_project.main.project_id
+  role    = "roles/storage.admin"
+  members = [
+    "principal://iam.googleapis.com/projects/${data.google_project.main.number}/locations/global/workloadIdentityPools/${data.google_project.main.project_id}.svc.id.goog/subject/ns/default/sa/mediamtx",
+  ]
+}
+
